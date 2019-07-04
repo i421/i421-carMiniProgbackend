b3b6117a8f8aa8ef6c5a98814d7cd617eb5c8208
shine1rainbow <shine1rainbow@gmail.com>
2019-07-04 11:35:45 +0800

    finished tag classify test

--- resources/assets/js/pages/Test.vue
+++ resources/assets/js/pages/Test.vue
@@ -1,12 +1,44 @@
 <template>
     <div>
-        <el-row>
-            test
-        </el-row>
+	  <div style="margin-top: 20px" v-for="(item,index,key) in tags" :key="key">
+		  <p>{{ index}}</p>
+		<el-radio-group v-model="selected[index]" size="small" @change="test">
+            <el-radio-button :label="v.id" v-for="(v, i, k) in item" :key="k">{{ v.name }}</el-radio-button>
+		</el-radio-group>
+	  </div>
     </div>
 </template>
 
 <script>
+  import { http } from './../utils/fetch'
+  import Api from './../config'
+  export default {
+	  data() {
+		return {
+			tags: [],
+			selected: [],
+        }
+      },
+
+	  created() {
+		this.fetchTag();
+	  },
+
+      methods: {
+        //获取角色列表
+        fetchTag () {
+            http({
+                url: Api.tag,
+            }).then(response => {
+				this.tags = response.data.data
+            })
+        },
+
+		test() {
+			console.log(this.selected)
+		}
+      }
+  }
 </script>
 
 <style>
